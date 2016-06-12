65c6bdd28b7b4f57bd46bd11ffc315ea636bce93
unknown <kuyu@KUYUV2004.fareast.corp.microsoft.com>
2016-06-12 18:31:50 +0800

    project files

---
+++ Microsoft.DataMining.Tools.ScopeUtils/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.DataMining.Tools.ScopeUtils
+{
+    public class Class1
+    {
+    }
+}
